27620dbd07cdb0f35efdecabe84c9398e371ee1d
Ghalib Butt <ghalibnbutt@gmail.com>
2022-12-15 19:36:53 +0500

    First commit

---
+++ src/app/basic-watch-schedule/basic-watch-schedule.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-watch-schedule',
+  templateUrl: './basic-watch-schedule.component.html',
+  styleUrls: ['./basic-watch-schedule.component.css']
+})
+export class BasicWatchScheduleComponent {
+
+}
